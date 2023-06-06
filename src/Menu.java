2108bb018a29819c863f08d9081e97dd17d14b31
washito0407 <washovilla78@gmail.com>
2023-06-06 09:28:37 -0800

    Calculo de volumenes

---
+++ src/Menu.java
@@ -0,0 +1,11 @@
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("   Menú de figuras geometricas   ");
+        System.out.println("OPCIONES:");
+        System.out.println("1) Calculo de un cubo");
+        System.out.println("2) Calculo de una pirámide");
+        System.out.println("3) Calculo de un cilindro");
+        System.out.println("4) Calculo de un prisma rectangular");
+
+    }
+}
