201852a2cd42273e4813a98e89e8be491541f893
NayeliBarrera <melani_nayhely@outlook.es>
2023-06-06 09:06:03 -0500

    hola

---
+++ src/PrismaRectangular.java
@@ -0,0 +1,12 @@
+public class PrismaRectangular {
+    //Atributos
+    int base = 5;
+    int altura = 10;
+    int profundidad = 15;
+    int areaBase = base * altura;
+    int volumen = areaBase * profundidad;
+        System.out.println("El volumen del prisma rectangular es: " + volumen);
+}
+
+
+
