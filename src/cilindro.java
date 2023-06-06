0415945be0298e0a6ea9b73f83a3279a0c971fbc
washito0407 <washovilla78@gmail.com>
2023-06-06 09:57:34 -0800

    Calculo de volumenes

--- src/cilindro.java
+++ src/cilindro.java
@@ -1,34 +1,41 @@
-
+import java.util.Scanner;
 public class cilindro {
-    float radio;
-    float altura;
+    double radio;
+    double altura;
 
-    public cilindro(float radio, float altura) {
+    public cilindro(double radio, double altura) {
         this.radio = radio;
         this.altura = altura;
-
     }
 
-    public float getRadio() {
+    public double getRadio() {
         return radio;
     }
 
-    public void setRadio(float radio) {
+    public void setRadio(double radio) {
         this.radio = radio;
     }
 
-    public float getAltura() {
+    public double getAltura() {
         return altura;
     }
 
-    public void setAltura(float altura) {
+    public void setAltura(double altura) {
         this.altura = altura;
     }
 
     public double volcilindro(){
+        Scanner input = new Scanner(System.in);
+        double radius = input.nextDouble();
+        double height = input.nextDouble();
 
+        double volume = calculateVolume(radius, height);
 
+        System.out.println("El volumen del cilindro es: " + volume);
+    }
 
-
+    public static double calculateVolume(double radius, double height) {
+        double volume = Math.PI * radius * radius * height;
+        return volume;
     }
 }
